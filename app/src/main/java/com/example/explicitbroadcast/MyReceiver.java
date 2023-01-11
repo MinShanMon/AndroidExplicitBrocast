6182be0e56fe84cfa0e4d0c4aa4a7b2c4cbf5c09
Your Name <shanmon2017@gmail.com>
2023-01-11 19:48:38 +0800

    explicit brocast

---
+++ app/src/main/java/com/example/explicitbroadcast/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.explicitbroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+    }
+}
